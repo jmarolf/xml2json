c09639b0abe1dcf519cdf9325c6705ced53f983c
Jonathon Marolf <jmarolf@microsoft.com>
2019-10-11 15:40:04 -0700

    initial import

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xml2json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
